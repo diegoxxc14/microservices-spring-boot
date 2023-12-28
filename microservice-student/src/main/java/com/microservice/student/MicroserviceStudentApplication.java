8a0389401a5cab3d60585cda69be0b3e80f2b067
Diego Cuenca <dcuenca@lorente.fin.ec>
2023-12-28 13:54:33 -0500

    Configuración mcsv Eureka y Student - Up OK

--- microservice-student/src/main/java/com/microservice/student/MicroserviceStudentApplication.java
+++ microservice-student/src/main/java/com/microservice/student/MicroserviceStudentApplication.java
@@ -2,7 +2,9 @@ package com.microservice.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient  // Indicar que será cliente de Eureka
 @SpringBootApplication
 public class MicroserviceStudentApplication {
 
